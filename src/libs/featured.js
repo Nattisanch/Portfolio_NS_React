d3addbf87406d584d3f9a9d3347be36579920ff5
Natalie Sanchez <nataliesanchez42204@gmail.com>
2025-11-04 06:19:20 -0500

    Changed example to tbd

--- src/libs/featured.js
+++ src/libs/featured.js
@@ -3,7 +3,7 @@
 const featured =[{
         title: "Career Navigator",
         description: "I created a career navigator for my liftoff project that helps users explore different career paths based on their interests and skills. The app provides personalized recommendations and resources to help users make informed decisions about their future.",
-        tech:"EXAMPLE: HTML5,CSS, JS",
+        tech:"TBD",
         link: './assets/placeholder.png',
         hard: " TBD.",
         easy: " TBD",
